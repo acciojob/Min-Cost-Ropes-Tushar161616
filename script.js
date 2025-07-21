6b180c89cba2ee42a197f92f4310fe595ed97304
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2025-07-21 23:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function mincost(arr) {
+   
+    let heap = new MinHeap();
+    for (let length of arr) {
+        heap.insert(length);
+    }
 
+    let totalCost = 0;
+
+
+    while (heap.size() > 1) {
+        // Step 3: Extract the two shortest ropes
+        let first = heap.extractMin();
+        let second = heap.extractMin();
+
+     
+        let cost = first + second;
+        totalCost += cost;
+
+     
+    }
+
+    // Step 6: Return the total cost
+    return totalCost;
+}
 module.exports=mincost;
